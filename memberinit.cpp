18552b36f660184d430a9bd1a5ff7747a9de0561
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-05-20 14:43:42 +0700

    membuat constructor di dalam akses public

--- memberinit.cpp
+++ memberinit.cpp
@@ -9,6 +9,11 @@ private:
 	float nilai;
 
 public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    { 
+    //Member initialization List
+	//Definisi
+	}
 
 };
 
